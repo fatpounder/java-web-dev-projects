e4f07fd0705168c84b5d91c680d77f521cf1726c
marie@FatPounder <marie5joiner@gmail.com>
2023-11-30 19:35:17 -0600

    studio with bonus

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -7,6 +7,21 @@ public class Area {
 
         Scanner input = new Scanner(System.in);
 
+//        System.out.println("Enter a radius");
+//        double PI = 3.14;
+//        if (input.hasNextDouble()) {
+//            double radius = input.nextDouble();
+//
+//            if (radius > 0) {
+//                double area = Circle.getArea(radius);
+//                System.out.println("The area of a circle of radius " + radius + " is: " + area);
+//            } else {
+//                System.out.println("Radius must be a positive number.");
+//            }
+//        } else {
+//            System.out.println("Please enter a valid number.");
+//        }
+
         double radius;
         do {
             System.out.println("Enter a number for the radius: ");
